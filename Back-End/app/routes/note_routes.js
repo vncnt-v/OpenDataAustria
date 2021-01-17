a7000a667a9d02649678b7fb9ec31b8cbe8f40b7
Vincent Vornkahl <if19b143@technikum-wien.at>
2021-01-17 21:12:39 +0100

    + impfung api

--- Back-End/app/routes/note_routes.js
+++ Back-End/app/routes/note_routes.js
@@ -1,7 +1,8 @@
 var corona = require('../http_requests/corona.js');
 var map = require('../http_requests/map.js');
-var corona = require('../http_requests/corona.js');
+var impfung = require('../http_requests/impfung.js');
 module.exports = function(app) {  
     app.get('/corona', (req, res) => { corona.getData(res)  });
+    app.get('/impfung', (req, res) => { impfung.getData(res)  });
     app.get('/map', (req, res) => { map.getData(res)  });
 };
